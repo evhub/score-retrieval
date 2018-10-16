f595e5a6038131dae1e61573581e98a3f2e5e455
Evan Hubinger <evanjhub@gmail.com>
2018-10-15 18:29:41 -0700

    Fix migration

--- score_retrieval/data.py
+++ score_retrieval/data.py
@@ -14,13 +14,13 @@ def save_first_page(pdf_path, img_path, dpi=300):
     pages[-1].save(img_path, os.path.splitext(img_path)[-1])
 
 
-def migrate_pdfs(img_ext="png", dpi=300):
+def migrate_pdfs(img_ext=".png", dpi=300):
     """Migrate all pdfs to images."""
     print("Migrating {}...".format(DATA_DIR))
     for dirpath, _, filenames in os.walk(DATA_DIR):
         for pdf_file in filenames:
             name, ext = os.path.splitext(pdf_file)
-            if ext == "pdf":
+            if ext == ".pdf":
                 img_file = name + "." + img_ext
                 if img_file in filenames:
                     print("Skipping {}...".format(img_file))
@@ -32,7 +32,7 @@ def migrate_pdfs(img_ext="png", dpi=300):
                     print("Saved {}.".format(img_file))
 
 
-def load_imgs(img_ext="png"):
+def load_imgs(img_ext=".png"):
     """Load all images in the dataset as an iterator."""
     for dirpath, _, filenames in os.walk(DATA_DIR):
         for fname in filenames:
