7f6c9488296e6a7ceb5f21ece27613c9dbd40bac
Evan Hubinger <evanjhub@gmail.com>
2018-10-24 16:37:08 -0700

    Improve data interface

--- score_retrieval/constants.py
+++ score_retrieval/constants.py
@@ -2,3 +2,7 @@ import os
 
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
+
+IMG_EXT = ".png"
+
+DPI = 300
--- score_retrieval/data.py
+++ score_retrieval/data.py
@@ -1,44 +1,75 @@
 import os
+from collections import defaultdict
 
 from pdf2image import convert_from_path
 from scipy.ndimage import imread
 
 from score_retrieval.constants import (
     DATA_DIR,
+    IMG_EXT,
+    DPI,
 )
 
 
-def save_first_page(pdf_path, img_path, dpi=300):
+def save_first_page(pdf_path, img_path):
     """Save first page of given pdf as an image."""
-    pages = convert_from_path(pdf_path, dpi)
+    pages = convert_from_path(pdf_path, DPI)
     pages[-1].save(img_path, os.path.splitext(img_path)[-1].lstrip("."))
 
 
-def migrate_pdfs(img_ext=".png", dpi=300):
+def migrate_pdfs():
     """Migrate all pdfs to images."""
     print("Migrating {}...".format(DATA_DIR))
     for dirpath, _, filenames in os.walk(DATA_DIR):
         for pdf_file in filenames:
             name, ext = os.path.splitext(pdf_file)
             if ext == ".pdf":
-                img_file = name + img_ext
+                img_file = name + IMG_EXT
                 if img_file in filenames:
                     print("Skipping {}...".format(img_file))
                 else:
                     print("Saving {}...".format(img_file))
                     pdf_path = os.path.join(dirpath, pdf_file)
                     img_path = os.path.join(dirpath, img_file)
-                    save_first_page(pdf_path, img_path, dpi)
+                    save_first_page(pdf_path, img_path)
                     print("Saved {}.".format(img_file))
 
 
-def load_imgs(img_ext=".png"):
-    """Load all images in the dataset as an iterator."""
+def index_images():
+    """Return an iterator of (label, path) for all images."""
     for dirpath, _, filenames in os.walk(DATA_DIR):
         for fname in filenames:
-            if os.path.splitext(fname)[-1] == img_ext:
-                img_path = os.path.join(dirpath, fname)
-                yield imread(img_path)
+            if os.path.splitext(fname)[-1] == IMG_EXT:
+                yield dirpath, os.path.join(dirpath, fname)
+
+
+def index_by_label():
+    """Return dictionary mapping labels to image paths."""
+    index = defaultdict(list)
+    for label, img_path in index_images():
+        index[label].append(img_path)
+    return index
+
+
+def index_data():
+    """Return database_paths, database_labels, query_paths, query_labels lists."""
+    database_paths = []
+    database_labels = []
+    query_paths = []
+    query_labels = []
+    for label, img_paths in index_by_label().items():
+        head, tail = img_paths[:-1], img_paths[-1]
+        database_paths += head
+        database_labels += [label]*len(head)
+        query_paths.append(tail)
+        query_paths.append(label)
+    return database_paths, database_labels, query_paths, query_labels
+
+
+def load_data():
+    """Return an iterator of (label, image) for all images."""
+    for label, img_path in index_images():
+        yield label, imread(img_path)
 
 
 if __name__ == "__main__":
