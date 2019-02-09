06607d8bc9fa885f69f908df71b336a126514c5e
Evan Hubinger <evanjhub@gmail.com>
2019-02-09 15:45:39 -0800

    Improve vec db

--- score_retrieval/vec_db.py
+++ score_retrieval/vec_db.py
@@ -14,18 +14,20 @@ from score_retrieval.constants import VECTOR_LEN
 
 
 def save_vecs(image_to_vec_func, dataset=None):
+    """Saves database of vectors using the given vector generation function."""
     for path, label in index_images(dataset):
         image = imread(path)
         vec = image_to_vec_func(image)
         assert vec.shape == (VECTOR_LEN,), "{}.shape != ({},)".format(vec.shape, VECTOR_LEN)
-        vec_path = os.path.splitext(path)[0] + ".pickle"
-        pickle.dump((label, vec), vec_path, protocol=pickle.HIGHEST_PROTOCOL)
+        vec_path = os.path.splitext(path)[0] + ".npy"
+        np.save(vec, vec_path)
 
 
 def load_vecs(image_paths):
+    """Yield vectors for the given image paths."""
     for path in image_paths:
-        pickle_path = os.path.splitext(path)[0] + ".pickle"
-        yield pickle.load(pickle_path, protocol=pickle.HIGHEST_PROTOCOL)
+        vec_path = os.path.splitext(path)[0] + ".npy"
+        yield np.load(vec_path)
 
 
 get_database_vecs = partial(load_vecs, database_paths)
