c3058a24138b3ced2d70225009e3fe13697f2f8e
Evan Hubinger <evanjhub@gmail.com>
2019-02-16 14:44:07 -0800

    Improve debug info

--- score_retrieval/vec_db.py
+++ score_retrieval/vec_db.py
@@ -51,9 +51,9 @@ def save_veclists(image_to_veclist_func, resample=False, normalize=False, datase
                     vec = resample_arr(vec)
                 if normalize:
                     vec = normalize_arr(vec)
-                print("Got vector of shape {}.".format(vec.shape))
                 veclist.append(vec)
         veclist = np.asarray(veclist)
+        print("veclist.shape =", veclist.shape)
 
         if isnull(veclist):
             print("Got null veclist for {} with shape {} (raw len {}).".format(path, veclist.shape, len(raw_veclist)))
