2619afce46da68713aa1ccf73875faba203bb7be
Evan Hubinger <evanjhub@gmail.com>
2019-02-23 19:07:37 -0800

    Use dot product

--- score_retrieval/retrieval.py
+++ score_retrieval/retrieval.py
@@ -39,7 +39,7 @@ def dot(vec1, vec2):
     return -np.dot(vec1.T, vec2)
 
 
-DIST_METRIC = L2
+DIST_METRIC = dot
 
 
 def retrieve_vec(query_vec, db_labels, db_vecs, db_inds):
@@ -55,11 +55,11 @@ def retrieve_vec(query_vec, db_labels, db_vecs, db_inds):
     return min_scores
 
 
-LIN_WEIGHT = 0.25
+LIN_WEIGHT = 0.75
 SLOPE_WEIGHT = 0.25
 
 
-def retrieve_veclist(query_veclist, db_labels, db_vecs, db_inds, debug=False):
+def retrieve_veclist(query_veclist, db_labels, db_vecs, db_inds, debug=True):
     """Find the label with the min sum of min dist and mean change
     in index for each vector."""
     # sum best distances into dist_scores and
