80151859386c59a97f6750e298cca9fe002ef79b
Evan Hubinger <evanjhub@gmail.com>
2019-02-28 19:19:03 -0800

    Use entire piano dataset

--- score_retrieval/constants.py
+++ score_retrieval/constants.py
@@ -2,6 +2,7 @@ import os
 import re
 
 
+# data copying constants
 SCRAPE_DIR = "/data1/dbashir/Project/score_scrape/results/composer"
 HTML_DIR = "/data1/dbashir/Project/results"
 HTML_EXT = ".txt"
@@ -9,21 +10,19 @@ SEARCH_HTML_FOR = re.compile(r"<th>Instrumentation\n</th>\n<td>.*Piano.*\n</td>"
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 
+# data migration constants
 IMG_EXT = ".png"
-
 DPI = 50
-
-MAX_QUERIES_PER_LABEL = 5
-
-# size of the subset of the data to sample
-SAMPLE = False
-
-VECTOR_LEN = 128
-
-DEFAULT_DATASET = "mini_dataset"
-
 START_PAGE = None
 END_PAGE = None
+
+# dataset constants
+DEFAULT_DATASET = "piano_dataset"
+MAX_QUERIES_PER_LABEL = None
+SAMPLE = False
+
+# retrieval constants
+VECTOR_LEN = 128
 
 
 def get_dataset_dir(dataset=None):
