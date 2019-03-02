98c3d1c1f2b37d979e31b14442c47df70cb8b476
Evan Hubinger <evanjhub@gmail.com>
2019-03-02 14:41:05 -0800

    Switch back to mini dataset

--- score_retrieval/constants.py
+++ score_retrieval/constants.py
@@ -17,7 +17,7 @@ START_PAGE = None
 END_PAGE = None
 
 # dataset constants
-DEFAULT_DATASET = "piano_dataset"
+DEFAULT_DATASET = "mini_dataset"
 MAX_QUERIES_PER_LABEL = None
 TEST_RATIO = 1.0
 TRAIN_RATIO = 0.0
