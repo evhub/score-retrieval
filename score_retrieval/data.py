945dad233484593bab40d46bc1bf66e810535f88
Evan Hubinger <evanjhub@gmail.com>
2019-03-12 18:06:15 -0700

    Fix duplicate image index

--- score_retrieval/data.py
+++ score_retrieval/data.py
@@ -86,7 +86,7 @@ def get_basename_to_path_dict(dataset=None):
     for _, path in index_images(dataset):
         basename = os.path.basename(path)
         if basename in basename_to_path:
-            raise IndexError("found duplicate image index {}".format(basename))
+            print("Found duplicate image index: {}".format(basename))
         basename_to_path[basename] = path
     return basename_to_path
 
