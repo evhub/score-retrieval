b8e89d5ef60395ef15aa80dfd5728c6bd378ea06
Evan Hubinger <evanjhub@gmail.com>
2019-04-03 00:03:47 -0700

    Set DPI back to 50

--- score_retrieval/constants.py
+++ score_retrieval/constants.py
@@ -21,7 +21,7 @@ SORT_HTML_BY = re.compile(r"<span class='current-rating' id='current-rating-\d+'
 # data migration constants
 IMG_EXT = ".png"
 NONE_DPI = 50
-DPI = 100
+DPI = 50
 
 # data renaming constants
 BASE_DATASET = "top50_piano_dataset"
